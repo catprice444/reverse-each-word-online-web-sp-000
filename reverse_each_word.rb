9813751a156c4be078d99b18a01ad84039cba3a3
Catherine Price <catprice444@gmail.com>
2020-04-23 16:25:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-statment.join = []
+array = string.split(" ")
+statment = []
 sentence.collect do |line|
-  statement << "line.reverse"
+  statement << line.reverse
 end
 end
